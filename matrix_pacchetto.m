0f2398963fe8cd67dc90d4168b6c91a73efd14e0
Howl <gabrielefogu@gmail.com>
2023-05-11 01:50:29 +0200

    buona parte

--- matrix_pacchetto.m
+++ matrix_pacchetto.m
@@ -1,142 +1,305 @@
 (* ::Package:: *)
 
+BeginPackage["Matrix`"]
+(*
+prodottoMatrici::usage = ""
+
+inputMatrice::usage = ""
+
+askKindOfMatrix::usage = ""
+
+askForDimensions::usage = ""
+
+generateRandomMatrix::usage = ""
+*)
+
+prodottoMatrici[A_, B_] := Module[{m, n, q, p, resultMatrix, i, j, k}, 
+{m, n} = Dimensions[A]; 
+{q, p} = Dimensions[B]; 
+    If[m != p, Print["Le dimensioni delle matrici non sono compatibili per il prodotto."]; Return[]]; resultMatrix = ConstantArray[0, {m, p}]; 
+    Do[resultMatrix[[i,j]] = Sum[Print["Calcolo ", A[[i,k]], " * ", B[[k,j]], " = ", A[[i,k]]*B[[k,j]]]; A[[i,k]]*B[[k,j]], {k, n}]; 
+      Print["La somma \[EGrave]: ", resultMatrix[[i,j]]], {i, m}, {j, p}]; resultMatrix]
+
+(*
+A = inputMatrice["A"]; 
+B = inputMatrice["B"];
+*)
+
+isValidDimension[n_]:= n != Null && IntegerQ[n] && 0 < n <= 10
+
+manipulateMatrixProduct[] := DynamicModule[{
+	 rowsA = 3,
+     colA = 3,
+     rowsB = 3,
+     colB = 3,
+	 matriceA = ConstantArray[0, {3, 3}],
+     matriceB = ConstantArray[0, {3, 3}],
+     risultato = "",
+     randomFill = False,
+     justUpdated = False,
+     seed = 0},
+    Manipulate[
+        If[randomFill,
+            SeedRandom[seed];
+            If[!justUpdated,
+                justUpdated = True;
+                rowsA = RandomChoice[{2, 3, 4, 5}];
+                colA = RandomChoice[{2, 3, 4, 5}];
+                rowsB = RandomChoice[{2, 3, 4, 5}];
+                colB = rowsA;
+            ];
+            matriceA = RandomInteger[{-10, 10}, {rowsA, colA}];
+            matriceB = RandomInteger[{-10, 10}, {rowsB, rowsA}],
+            seed="";
+            justUpdated = False;
+            matriceA = ConstantArray[0, {rowsA, colA}];
+            matriceB = ConstantArray[0, {rowsB, colB}]
+        ];
+        
+        Column[{
+            Row[{"Dimensione Matrice A:",
+            (*
+	            Chiedere come validare il range, 
+	            If[isValidDimension[rowsA], rowsA = #] &, 
+	            non funziona, lo stesso vale per il seed: InputField non accetta range
+            *)
+                InputField[Dynamic[rowsA], 
+                Number, FieldSize -> {2, 1}, Alignment -> Center, Enabled -> !randomFill],
+                "x",
+                InputField[Dynamic[colA], 
+                Number, FieldSize -> {2, 1}, Alignment -> Center, Enabled -> !randomFill]
+            }],
+            Row[{"Dimensione Matrice B:",
+                InputField[Dynamic[rowsB], 
+                Number, FieldSize -> {2, 1}, Alignment -> Center, Enabled -> !randomFill],
+                "x",
+                InputField[Dynamic[colB], 
+                Number, FieldSize -> {2, 1}, Alignment -> Center, Enabled -> !randomFill]
+            }, Alignment->Center],
+            Row[Spacer[20]{
+	            Column[{
+	                "Matrice A:",
+	                Dynamic@Grid[
+	                    If[randomFill,
+	                        matriceA,
+	                        Table[
+	                            With[{i = i, j = j},
+	                                InputField[Dynamic[matriceA[[i, j]]], Number, FieldSize -> {3, 1}, Alignment -> Center]
+	                            ],
+	                            {i, rowsA}, {j, colA}
+	                        ]
+	                    ],
+	                    Frame -> All, Spacings -> {1, 1}, ItemSize -> {3, 2}
+	                ]
+	            }, Alignment->Center],
+	            Column[{
+	                "Matrice B:",
+	                Dynamic@Grid[
+	                    If[randomFill,
+	                        matriceB,
+	                        Table[
+	                            With[{i = i, j = j},
+	                                InputField[Dynamic[matriceB[[i, j]]], Number, FieldSize -> {3, 1}, Alignment -> Center]
+	                            ],
+	                            {i, rowsB}, {j, colB}
+	                        ]
+	                    ],
+	                    Frame -> All, Spacings -> {1, 1}, ItemSize -> {3, 2}
+	                ]
+	            }, Alignment->Center]
+            }],
+		
+		Row[{
+			Button["Calcola prodotto", 
+				risultato=MatrixForm[matriceA . matriceB]
+			],
+			Button["Reset",
+				seed="";
+				randomFill=False;
+				matriceA=ConstantArray[0,{rowsA,colA}];
+				matriceB=ConstantArray[0,{rowsB,colB}];
+				risultato=""
+			]
+		}],
+		Row[{"Risultato:",risultato}]
+	}],
+	Row[ Spacer[20]{
+		Control[{
+			{randomFill,False,"Riempi randomicamente"},{False,True}
+		}],
+		Dynamic@If[randomFill,
+			Column[{Text["Random Seed : " Green], InputField[Dynamic[seed], Number, FieldSize -> {10, 1.5}]}],
+            Column[{Text["Random Seed : " Red], InputField[Dynamic[seed], Number, FieldSize -> {10, 1.5}, Enabled -> False]}]
+        ]
+	}],
+	
+	TrackedSymbols:>{rowsA, colA, rowsB, colB, randomFill, seed},
+	SynchronousUpdating->True
+	]
+]
+
+manipulateMatrixProduct[]
+EndPackage[]
+
+
+
+
+
 (* ::Input:: *)
-(*BeginPackage["Matrix`"]*)
-(*(**)
-(*prodottoMatrici::usage = ""*)
-(**)
-(*inputMatrice::usage = ""*)
-(**)
-(*askKindOfMatrix::usage = ""*)
-(**)
-(*askForDimensions::usage = ""*)
-(**)
-(*generateRandomMatrix::usage = ""*)
-(**)*)
-(**)
-(*prodottoMatrici[A_, B_] := Module[{m, n, q, p, resultMatrix, i, j, k}, *)
-(*{m, n} = Dimensions[A]; *)
-(*{q, p} = Dimensions[B]; *)
-(*    If[m != p, Print["Le dimensioni delle matrici non sono compatibili per il prodotto."]; Return[]]; resultMatrix = ConstantArray[0, {m, p}]; *)
-(*    Do[resultMatrix[[i,j]] = Sum[Print["Calcolo ", A[[i,k]], " * ", B[[k,j]], " = ", A[[i,k]]*B[[k,j]]]; A[[i,k]]*B[[k,j]], {k, n}]; *)
-(*      Print["La somma \[EGrave]: ", resultMatrix[[i,j]]], {i, m}, {j, p}]; resultMatrix]*)
-(**)
-(*inputMatrice[name_] := Module[{n, m, messaggio, inserimentoRighe, inserimentoColonne, valore, matrice}, *)
-(*  inserimentoRighe = StringJoin["Inserisci il numero di righe della matrice ", name, ": "]; *)
-(*  inserimentoColonne = StringJoin["Inserisci il numero di colonne della matrice ", name, ": "]; *)
-(*  While[ !NumericQ[n], Print["Errore: Inserisci un valore numerico."]; *)
-(*	n = Input[inserimentoRighe]; *)
-(*]; *)
-(*  While[ !NumericQ[m], Print["Errore: Inserisci un valore numerico."]; *)
-(*m = Input[inserimentoColonne]; *)
-(*]; *)
-(*  Print[StringJoin["Numero di righe della matrice ", name, " che hai inserito: "], n]; *)
-(*  Print[StringJoin["Numero di colonne della matrice ", name, " che hai inserito: "], m]; Print["Creazione della matrice vuota"]; *)
-(*  matrice = ConstantArray[0, {n, m}]; *)
-(*Do[messaggio = StringJoin["Inserisci il valore per l'elemento (", ToString[i], ",", ToString[j], "): "]*)
-(*valore = Input[messaggio]; *)
-(*   While[ !NumericQ[valore], *)
-(*	Print["Errore: Inserisci un valore numerico."]; valore = Input[messaggio]; *)
-(*  ]; *)
-(*matrice[[i,j]] = valore; , {i, n}, {j, m}*)
-(*]; *)
-(*matrice; *)
-(*Print[StringJoin["Matrice ", name, " creata."]]; *)
-(*Print[matrice]; *)
-(*Return[matrice]]*)
-(**)
-(*generateRandomMatrix[r1c2_, c1_, r2_] := Module[{seed, matriceA, matriceB}, *)
-(*(*Chiedere, vuoi riempire la matrice o la vuoi random?*)*)
-(*seed = Input["Inserisci il valore del seed (compreso tra 0 e 10000): "]; *)
-(*SeedRandom[seed]; *)
-(*  Print["Il seed \[EGrave]: "]; *)
-(*Print[seed]; *)
-(*matriceA = RandomInteger[{0, 10}, {r1c2, c1}]; *)
-(*matriceB = RandomInteger[{0, 10}, {r2, r1c2}]; *)
-(*  Print["Matrice randomica A: "]; *)
-(*Print[matriceA]; *)
-(*Print["Stampa griglia della matrice A: "]; *)
-(*Grid[matriceA, Frame -> All]; *)
-(*  Print["Matrice randomica B: "]; *)
-(*Print[matriceB]; *)
-(*Print["Stampa griglia della matrice B: "]; *)
-(*Grid[matriceB, Frame -> All]; *)
-(*    {matriceA, matriceB}]*)
-(**)
-(**)
-(*(**)
-(*A = inputMatrice["A"]; *)
-(*B = inputMatrice["B"];*)
-(**)*)
-(*(* chiamo la funzione per mostrare gli input all'utente *)*)
-(*(*DynamicModule[{righe1, colonne1, righe2, colonne2},*)*)
-(**)
-(*askKindOfMatrix[]:=DynamicModule[{random=False},*)
-(*CreateDialog[*)
-(*Column[{*)
-(*TextCell["Preferisci esercitarti con matrici riempite randomicamente oppure con matrici a tua scelta?"],*)
-(*Row[{*)
-(*Button["Random",*)
-(*random=True; *)
-(*DialogReturn[]*)
-(*],*)
-(*Button["Custom",*)
-(*random=False;*)
-(*DialogReturn[]*)
-(*]*)
-(*}, Alignment->Center],*)
-(*DefaultButton[]}*)
-(*],Modal->True];*)
-(*If[random,Print["Random"],Print["Custom"]]]*)
-(**)
-(**)
-(**)
-(*askForDimensions[] := DynamicModule[{righe1, colonne1, righe2, colonne2},*)
-(*Grid[{*)
-(*	   { "Prima matrice - Inserisci valori fra 1 e 5:", SpanFromLeft },*)
-(*	   { *)
-(*	InputField[Dynamic[righe1], Number, FieldSize -> {15, 1.5}, Alignment -> Center, FieldHint -> "Righe prima matrice"],*)
-(*	InputField[Dynamic[colonne1], Number, FieldSize -> {15, 1.5}, Alignment -> Center, FieldHint -> "Colonne prima matrice"] *)
-(* },*)
-(*	    { "Seconda matrice - Inserisci valori fra 1 e 5:", SpanFromLeft },*)
-(*	    { *)
-(*	InputField[Dynamic[righe2], Number, FieldSize -> {15, 1.5}, Alignment -> Center, FieldHint -> "Righe seconda matrice"],*)
-(*	InputField[Dynamic[colonne2], Number, FieldSize -> {15, 1.5}, Alignment -> Center, FieldHint -> "Colonne seconda matrice"] *)
-(* },*)
-(*	    { *)
-(*	SpanFromLeft, *)
-(*	Button["Valida le dimensioni!",*)
-(*			If[IntegerQ[righe1] && 1 <= righe1 <= 5 && IntegerQ[colonne1] && 1 <= colonne1 <= 5 *)
-(*&&IntegerQ[righe2] && 1 <= righe2 <= 5 && IntegerQ[colonne2] && 1 <= colonne2 <= 5,*)
-(*		If[righe1 != colonne2, *)
-(*					MessageDialog["Il numero di righe della prima matrice e di colonne della seconda deve coincidere"];*)
-(*					Return askForDimensions[],*)
-(*					askKindOfMatrix[]*)
-(*		],	 *)
-(*				MessageDialog["Le dimensioni inserite non sono valide! Ricorda di inserire valori da 1 a 5."]*)
-(*			]*)
-(*	]*)
-(*	    }*)
-(*}]*)
-(*]*)
-(**)
-(*askForDimensions[]*)
-(*(*randomMatrici[righe, colonne]*)
-(*result = prodottoMatrici[A, B]; *)
-(**)
-(*Print[result]; *)
-(*Grid[{{Grid[A, Frame -> All], "x", Grid[B, Frame -> All], "=", Grid[result, Frame -> All]}}]*)
-(**)*)
-(*EndPackage[]*)
+(*Manipulate[If[Matrix`randomFill, Matrix`seed$$ = ""; *)
+(*     If[ !Matrix`justUpdated$$, Matrix`justUpdated$$ = True; *)
+(*       Matrix`rowsA$$ = RandomChoice[{2, 3, 4, 5}]; Matrix`colA$$ = *)
+(*        RandomChoice[{2, 3, 4, 5}]; Matrix`rowsB$$ = *)
+(*        RandomChoice[{2, 3, 4, 5}]; Matrix`colB$$ = Matrix`rowsA$$; ]; *)
+(*     Matrix`matriceA$$ = RandomInteger[{-10, 10}, {Matrix`rowsA$$, *)
+(*        Matrix`colA$$}]; Matrix`matriceB$$ = RandomInteger[{-10, 10}, *)
+(*       {Matrix`rowsB$$, Matrix`rowsA$$}], Matrix`justUpdated$$ = False; *)
+(*     Matrix`matriceA$$ = ConstantArray[0, {Matrix`rowsA$$, Matrix`colA$$}]; *)
+(*     Matrix`matriceB$$ = ConstantArray[0, {Matrix`rowsB$$, Matrix`colB$$}]]; *)
+(*   Column[{Row[{"Dimensione Matrice A:", InputField[Dynamic[Matrix`rowsA$$, *)
+(*         If[#1 != Null && NumericQ[#1] && 0 < #1 < 10, Matrix`rowsA$$ = *)
+(*            #1] & ], Number, FieldSize -> {2, 1}, Alignment -> Center, *)
+(*        Enabled ->  !Matrix`randomFill], "x", InputField[*)
+(*        Dynamic[Matrix`colA$$, If[#1 != Null && NumericQ[#1] && 0 < #1 < 10, *)
+(*           Matrix`colA$$ = #1] & ], Number, FieldSize -> {2, 1}, *)
+(*        Alignment -> Center, Enabled ->  !Matrix`randomFill]}], *)
+(*     Row[{"Dimensione Matrice B:", InputField[Dynamic[Matrix`rowsB$$, *)
+(*         If[#1 != Null && NumericQ[#1] && 0 < #1 < 10, Matrix`rowsB$$ = *)
+(*            #1] & ], Number, FieldSize -> {2, 1}, Alignment -> Center, *)
+(*        Enabled ->  !Matrix`randomFill], "x", InputField[*)
+(*        Dynamic[Matrix`colB$$, If[#1 != Null && NumericQ[#1] && 0 < #1 < 10, *)
+(*           Matrix`colB$$ = #1] & ], Number, FieldSize -> {2, 1}, *)
+(*        Alignment -> Center, Enabled ->  !Matrix`randomFill]}, *)
+(*      Alignment -> Center], *)
+(*     Row[Spacer[20]*{Column[{"Matrice A:", Dynamic[*)
+(*           Grid[If[Matrix`randomFill, Matrix`matriceA$$, *)
+(*             Table[With[{Matrix`i$ = Matrix`i, Matrix`j$ = Matrix`j}, *)
+(*               InputField[Dynamic[Matrix`matriceA$$[[Matrix`i$,Matrix`j$]]], *)
+(*                Number, FieldSize -> {3, 1}, Alignment -> Center]], *)
+(*              {Matrix`i, Matrix`rowsA$$}, {Matrix`j, Matrix`colA$$}]], *)
+(*            Frame -> All, Spacings -> {1, 1}, ItemSize -> {3, 2}]]}, *)
+(*         Alignment -> Center], Column[{"Matrice B:", *)
+(*          Dynamic[Grid[If[Matrix`randomFill, Matrix`matriceB$$, *)
+(*             Table[With[{Matrix`i$ = Matrix`i, Matrix`j$ = Matrix`j}, *)
+(*               InputField[Dynamic[Matrix`matriceB$$[[Matrix`i$,Matrix`j$]]], *)
+(*                Number, FieldSize -> {3, 1}, Alignment -> Center]], *)
+(*              {Matrix`i, Matrix`rowsB$$}, {Matrix`j, Matrix`colB$$}]], *)
+(*            Frame -> All, Spacings -> {1, 1}, ItemSize -> {3, 2}]]}, *)
+(*         Alignment -> Center]}], *)
+(*     Row[{Button["Calcola prodotto", Matrix`risultato$$ = *)
+(*         MatrixForm[Matrix`matriceA$$ . Matrix`matriceB$$]], *)
+(*       Button["Azzera", Matrix`matriceA$$ = ConstantArray[0, *)
+(*           {Matrix`rowsA$$, Matrix`colA$$}]; Matrix`matriceB$$ = *)
+(*          ConstantArray[0, {Matrix`rowsB$$, Matrix`colB$$}]; *)
+(*         Matrix`risultato$$ = ""]}], *)
+(*     Row[{"Risultato:", Matrix`risultato$$}]}], *)
+(*  {{Matrix`randomFill, False, "Riempi randomicamente"}, {False, True}, *)
+(*   ControlPlacement -> 1}, Row[{Manipulate`Place[1]*Spacer[20], *)
+(*    Dynamic[If[Matrix`randomFill, Column[{Text["Random Seed : "*Green], *)
+(*         InputField[Dynamic[Matrix`seed$$], String, FieldSize -> *)
+(*           {10, 1.5}]}], Column[{Text["Random Seed : "*Red], *)
+(*         InputField[Dynamic[Matrix`seed$$], String, FieldSize -> {10, 1.5}, *)
+(*          Enabled -> False]}]]]*Spacer[20]}], *)
+(*  TrackedSymbols :> {Matrix`rowsA$$, Matrix`colA$$, Matrix`rowsB$$, *)
+(*    Matrix`colB$$, Matrix`randomFill}, SynchronousUpdating -> False]*)
+
+
+(* ::Input:: *)
+(*Manipulate[If[Matrix`randomFill, Matrix`seed$$ = ""; *)
+(*     If[ !Matrix`justUpdated$$, Matrix`justUpdated$$ = True; *)
+(*       Matrix`rowsA$$ = RandomChoice[{2, 3, 4, 5}]; Matrix`colA$$ = *)
+(*        RandomChoice[{2, 3, 4, 5}]; Matrix`rowsB$$ = *)
+(*        RandomChoice[{2, 3, 4, 5}]; Matrix`colB$$ = Matrix`rowsA$$; ]; *)
+(*     Matrix`matriceA$$ = RandomInteger[{-10, 10}, {Matrix`rowsA$$, *)
+(*        Matrix`colA$$}]; Matrix`matriceB$$ = RandomInteger[{-10, 10}, *)
+(*       {Matrix`rowsB$$, Matrix`rowsA$$}], Matrix`justUpdated$$ = False; *)
+(*     Matrix`matriceA$$ = ConstantArray[0, {Matrix`rowsA$$, Matrix`colA$$}]; *)
+(*     Matrix`matriceB$$ = ConstantArray[0, {Matrix`rowsB$$, Matrix`colB$$}]]; *)
+(*   Column[{Row[{"Dimensione Matrice A:", InputField[Dynamic[Matrix`rowsA$$, *)
+(*         If[#1 != Null && NumericQ[#1] && 0 < #1 < 10, Matrix`rowsA$$ = *)
+(*            #1] & ], Number, FieldSize -> {2, 1}, Alignment -> Center, *)
+(*        Enabled ->  !Matrix`randomFill], "x", InputField[*)
+(*        Dynamic[Matrix`colA$$, If[#1 != Null && NumericQ[#1] && 0 < #1 < 10, *)
+(*           Matrix`colA$$ = #1] & ], Number, FieldSize -> {2, 1}, *)
+(*        Alignment -> Center, Enabled ->  !Matrix`randomFill]}], *)
+(*     Row[{"Dimensione Matrice B:", InputField[Dynamic[Matrix`rowsB$$, *)
+(*         If[#1 != Null && NumericQ[#1] && 0 < #1 < 10, Matrix`rowsB$$ = *)
+(*            #1] & ], Number, FieldSize -> {2, 1}, Alignment -> Center, *)
+(*        Enabled ->  !Matrix`randomFill], "x", InputField[*)
+(*        Dynamic[Matrix`colB$$, If[#1 != Null && NumericQ[#1] && 0 < #1 < 10, *)
+(*           Matrix`colB$$ = #1] & ], Number, FieldSize -> {2, 1}, *)
+(*        Alignment -> Center, Enabled ->  !Matrix`randomFill]}, *)
+(*      Alignment -> Center], *)
+(*     Row[Spacer[20]*{Column[{"Matrice A:", Dynamic[*)
+(*           Grid[If[Matrix`randomFill, Matrix`matriceA$$, *)
+(*             Table[With[{Matrix`i$ = Matrix`i, Matrix`j$ = Matrix`j}, *)
+(*               InputField[Dynamic[Matrix`matriceA$$[[Matrix`i$,Matrix`j$]]], *)
+(*                Number, FieldSize -> {3, 1}, Alignment -> Center]], *)
+(*              {Matrix`i, Matrix`rowsA$$}, {Matrix`j, Matrix`colA$$}]], *)
+(*            Frame -> All, Spacings -> {1, 1}, ItemSize -> {3, 2}]]}, *)
+(*         Alignment -> Center], Column[{"Matrice B:", *)
+(*          Dynamic[Grid[If[Matrix`randomFill, Matrix`matriceB$$, *)
+(*             Table[With[{Matrix`i$ = Matrix`i, Matrix`j$ = Matrix`j}, *)
+(*               InputField[Dynamic[Matrix`matriceB$$[[Matrix`i$,Matrix`j$]]], *)
+(*                Number, FieldSize -> {3, 1}, Alignment -> Center]], *)
+(*              {Matrix`i, Matrix`rowsB$$}, {Matrix`j, Matrix`colB$$}]], *)
+(*            Frame -> All, Spacings -> {1, 1}, ItemSize -> {3, 2}]]}, *)
+(*         Alignment -> Center]}], *)
+(*     Row[{Button["Calcola prodotto", Matrix`risultato$$ = *)
+(*         MatrixForm[Matrix`matriceA$$ . Matrix`matriceB$$]], *)
+(*       Button["Azzera", Matrix`matriceA$$ = ConstantArray[0, *)
+(*           {Matrix`rowsA$$, Matrix`colA$$}]; Matrix`matriceB$$ = *)
+(*          ConstantArray[0, {Matrix`rowsB$$, Matrix`colB$$}]; *)
+(*         Matrix`risultato$$ = ""]}], *)
+(*     Row[{"Risultato:", Matrix`risultato$$}]}], *)
+(*  {{Matrix`randomFill, False, "Riempi randomicamente"}, {False, True}, *)
+(*   ControlPlacement -> 1}, Row[{Manipulate`Place[1]*Spacer[20], *)
+(*    Dynamic[If[Matrix`randomFill, Column[{Text["Random Seed : "*Green], *)
+(*         InputField[Dynamic[Matrix`seed$$], String, FieldSize -> *)
+(*           {10, 1.5}]}], Column[{Text["Random Seed : "*Red], *)
+(*         InputField[Dynamic[Matrix`seed$$], String, FieldSize -> {10, 1.5}, *)
+(*          Enabled -> False]}]]]*Spacer[20]}], *)
+(*  TrackedSymbols :> {Matrix`rowsA$$, Matrix`colA$$, Matrix`rowsB$$, *)
+(*    Matrix`colB$$, Matrix`randomFill}, SynchronousUpdating -> False]*)
+
+
+33
+
+
+(* ::Input:: *)
+(*Manipulate[If[Matrix`randomFill, *)
+(*    Matrix`matriceA$$ = RandomInteger[{0, 10}, {Matrix`dimensioneA, *)
+(*        Matrix`dimensioneA}]; Matrix`matriceB$$ = RandomInteger[{0, 10}, *)
+(*       {Matrix`dimensioneB, Matrix`dimensioneB}]; Matrix`inputEnabled$$ = *)
+(*      False; , Matrix`matriceA$$ = ConstantArray[0, {Matrix`dimensioneA, *)
+(*        Matrix`dimensioneA}]; Matrix`matriceB$$ = ConstantArray[0, *)
+(*       {Matrix`dimensioneB, Matrix`dimensioneB}]; Matrix`inputEnabled$$ = *)
+(*      True; ]; Column[{Row[{"Matrice A:", Grid[Matrix`matriceA$$, *)
+(*        Frame -> All]}], Row[{"Matrice B:", Grid[Matrix`matriceB$$, *)
+(*        Frame -> All]}], Row[{Button["Calcola prodotto", *)
+(*        Matrix`risultato$$ = MatrixForm[Matrix`matriceA$$ . *)
+(*           Matrix`matriceB$$]], Button["Reset", *)
+(*        Matrix`matriceA$$ = ConstantArray[0, {Matrix`dimensioneA, *)
+(*            Matrix`dimensioneA}]; Matrix`matriceB$$ = ConstantArray[0, *)
+(*           {Matrix`dimensioneB, Matrix`dimensioneB}]; Matrix`risultato$$ = *)
+(*          ""]}], Row[{"Risultato:", Matrix`risultato$$}]}], *)
+(*  {{Matrix`dimensioneA, 2}, {2, 3, 4}, ControlType -> SetterBar}, *)
+(*  {{Matrix`dimensioneB, 2}, {2, 3, 4}, ControlType -> SetterBar}, *)
+(*  {{Matrix`randomFill, True, "Riempi randomicamente"}, {False, True}}, *)
+(*  ControlPlacement -> Left, TrackedSymbols :> {Matrix`dimensioneA, *)
+(*    Matrix`dimensioneB, Matrix`randomFill}]*)
+
+
+(* ::Input:: *)
+(*"Matrix`"*)
 
 
 (* ::Input:: *)
 (**)
 
 
-
-
-
+(* ::Input:: *)
+(**)
 
 
 (* ::Input:: *)
